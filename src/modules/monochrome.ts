3278da2d2f10c482d09f1330dda65e9e477b6d25
ISIGE, Akira <xia@silvia.com>
2018-11-24 20:26:43 +0900

    bodyタグのみfilterかけるのでもわりと大丈夫そう……？

--- src/modules/monochrome.ts
+++ src/modules/monochrome.ts
@@ -2,64 +2,11 @@ import {Rgb} from './rgb';
 
 export class Monochrome {
     static monochromize() {
-        const root = document.documentElement;
-        if (root) {
-            this.monochromizeElement(root);
+        const body = document.querySelector('body');
+        if (body) {
+            this.applyGrayscaleFilter(body);
+            this.applyMonochromeColor(body);
         }
-    }
-
-    /**
-     * エレメントをモノクロ化して子も全部やる．
-     * @param element
-     */
-    static monochromizeElement(element: HTMLElement) {
-        if (this.inspectColored(element)) {
-            // グレースケールフィルタを追加．大体これで足りる．
-            this.applyGrayscaleFilter(element);
-
-            // bodyタグは何故かfilterが効かない？ 仕方ないので色を変更
-            if (element.tagName.toUpperCase() === 'BODY') {
-                this.applyMonochromeColor(element);
-            }
-        }
-        // 子要素全部に再帰で同じ処理．
-        const children = element.children;
-        const length = element.children.length;
-        for (let i = 0; i < length; ++i) {
-            this.monochromizeElement(children[i] as HTMLElement);
-        }
-    }
-
-    /**
-     * 対象エレメントが何らか色の付いたものであるかどうかを調査
-     * @param element
-     */
-    private static inspectColored(element: HTMLElement): boolean {
-        // 特定タグは内容にかかわらず無条件
-        if (['IMG', 'IFRAME'].includes(element.tagName.toUpperCase())) {
-            // console.log([`${element.tagName}: 画像`, element]);
-            return true;
-        }
-        for (const pseudElt of ['', '::before', '::after']) {
-            const style = window.getComputedStyle(element, pseudElt);
-            // バックグラウンド画像あり
-            if (style.backgroundImage && style.backgroundImage !== 'none') {
-                // console.log([`${element.tagName}: 背景画像 "${style.backgroundImage}"`, element]);
-                return true;
-            }
-            // 色が付きそうな要素のどれが一つでも色付き
-            const styleNames: (keyof CSSStyleDeclaration)[] = [
-                'color', 'backgroundColor',
-                'borderColor', 'borderBottomColor', 'borderLeftColor', 'borderRightColor', 'borderTopColor',
-            ];
-            for (const styleName of styleNames) {
-                if (style[styleName] && ! Rgb.parse(style[styleName]).isMonochrome()) {
-                    // console.log([`${element.tagName}: ${styleName} "${style[styleName]}"`, element]);
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     /**
@@ -81,15 +28,20 @@ export class Monochrome {
      */
     private static applyMonochromeColor(element: HTMLElement): void {
         const style = window.getComputedStyle(element);
+        console.log([style.backgroundColor, style.color, style.backgroundImage]);
         if (style.backgroundColor) {
             const rgb = Rgb.parse(style.backgroundColor);
-            element.style.backgroundColor = rgb.monochromed().toString();
-            element.dataset.monochromerModified = 'modified';
+            if (! rgb.isMonochrome()) {
+                element.style.backgroundColor = rgb.monochromed().toString();
+                element.dataset.monochromerModified = 'modified';
+            }
         }
         if (style.color) {
             const rgb = Rgb.parse(style.color);
-            element.style.color = rgb.monochromed().toString();
-            element.dataset.monochromerModified = 'modified';
+            if (! rgb.isMonochrome()) {
+                element.style.color = rgb.monochromed().toString();
+                element.dataset.monochromerModified = 'modified';
+            }
         }
         if (style.backgroundImage) {
             element.style.backgroundImage = '';
