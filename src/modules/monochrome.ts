a7ec6bdb2018d7be8799dde89127b50a4f1c4d8e
ISIGE, Akira <xia@silvia.com>
2018-11-24 20:45:19 +0900

    ボタンのトグルでモノクロ化を解除できる機能を実装

--- src/modules/monochrome.ts
+++ src/modules/monochrome.ts
@@ -4,8 +4,15 @@ export class Monochrome {
     static monochromize() {
         const body = document.querySelector('body');
         if (body) {
-            this.applyGrayscaleFilter(body);
-            this.applyMonochromeColor(body);
+            if (body.dataset.monochromerModified != 'modified') {
+                // モノクロ化する
+                this.applyGrayscaleFilter(body);
+                this.applyMonochromeColor(body);
+            } else {
+                // 戻す
+                this.ceaseGrayscaleFilter(body);
+                this.ceaseMonochromeColor(body);
+            }
         }
     }
 
@@ -18,8 +25,18 @@ export class Monochrome {
         if (filter && filter !== 'none') {
             console.log(`既存filter "${filter}"`);
         }
+        element.dataset.monochromerFilter = element.style.filter || '';
         element.style.filter = 'grayscale(1)';
         element.dataset.monochromerModified = 'modified';
+    }
+
+    /**
+     * CSS filter によるモノクロ化を解除
+     * @param element
+     */
+    private static ceaseGrayscaleFilter(element: HTMLElement): void {
+        element.style.filter = element.dataset.monochromerFilter || '';
+        element.dataset.monochromerModified = '';
     }
 
     /**
@@ -32,6 +49,7 @@ export class Monochrome {
         if (style.backgroundColor) {
             const rgb = Rgb.parse(style.backgroundColor);
             if (! rgb.isMonochrome()) {
+                element.dataset.monochromerBackgroundColor = style.backgroundColor;
                 element.style.backgroundColor = rgb.monochromed().toString();
                 element.dataset.monochromerModified = 'modified';
             }
@@ -39,13 +57,35 @@ export class Monochrome {
         if (style.color) {
             const rgb = Rgb.parse(style.color);
             if (! rgb.isMonochrome()) {
+                element.dataset.monochromerColor = style.color;
                 element.style.color = rgb.monochromed().toString();
                 element.dataset.monochromerModified = 'modified';
             }
         }
         if (style.backgroundImage) {
+            element.dataset.monochromerBackgroundImage = style.backgroundImage;
             element.style.backgroundImage = '';
             element.dataset.monochromerModified = 'modified';
         }
+    }
+
+    /**
+     * color/background-color 直接変更によるモノクロ化を解除
+     * @param element
+     */
+    private static ceaseMonochromeColor(element: HTMLElement): void {
+        const backgroundColor = element.dataset.monochromerBackgroundColor;
+        if (backgroundColor) {
+            element.style.backgroundColor = backgroundColor;
+        }
+        const color = element.dataset.monochromerColor;
+        if (color) {
+            element.style.color = color;
+        }
+        const backgroundImage = element.dataset.monochromerBackgroundImage;
+        if (backgroundImage) {
+            element.style.backgroundImage= backgroundImage;
+        }
+        element.dataset.monochromerModified = '';
     }
 }
